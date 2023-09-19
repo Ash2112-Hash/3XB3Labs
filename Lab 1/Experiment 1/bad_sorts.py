d51ccf6fc80dbc3a8faeff5e307a9f5c30c5280e
Ashwin Unnithan <unnithanash@gmail.com>
2023-09-19 17:54:51 -0400

    Add Insertion Sort Function

--- Lab 1/Experiment 1/bad_sorts.py
+++ Lab 1/Experiment 1/bad_sorts.py
@@ -2,8 +2,10 @@
 This file corresponds to the first graded lab of 2XC3.
 Feel free to modify and/or add functions to this file.
 """
+"""Import the following libraries required for the sort and experiment functions"""
 import random
-
+import timeit
+import matplotlib.pyplot as plot
 
 # Create a random list length "length" containing whole numbers between 0 and max_value inclusive
 def create_random_list(length, max_value):
@@ -68,3 +70,41 @@ def find_min_index(L, n):
         if L[i] < L[min_index]:
             min_index = i
     return min_index
+
+
+
+
+# ******************* Experiment 1 Functions *******************
+
+# test1_InsertionSort tests the runtime of insertion sort against different and increasing list sizes (starts initially at 100)
+# add more comments
+def test1_InsertionSort(test_runs, average_runs, increaseList_factor):
+    list_max = 100
+    list_len = 100
+    times = []
+    list_lens = []
+
+    for num in range(test_runs):
+        L = create_random_list(list_len, list_max)
+        list_lens.append(len(L))
+        time_sum = 0
+
+        for i in range(average_runs):
+            start = timeit.default_timer()
+            insertion_sort(L)
+            end = timeit.default_timer()
+            time_sum += (end-start)
+
+        times.append(time_sum/average_runs)
+        list_len += increaseList_factor
+        print(times)
+
+    plot.plot(list_lens, times, marker='o')
+    plot.xlabel('List Length')
+    plot.ylabel('Runtime (seconds)')
+    plot.title('Insertion Sort Runtime vs List Length')
+    plot.show()
+    return list_lens, times
+
+
+test1_InsertionSort(10, 10, 1000)
