b994b262a8629e6a514e3c1ab0924e37c022217d
Ashwin Unnithan <unnithanash@gmail.com>
2023-09-21 16:55:09 -0400

    Format Exp1

--- Lab 1/Experiment 1/Experiment 1.py
+++ Lab 1/Experiment 1/Experiment 1.py
@@ -2,7 +2,19 @@
 
 from bad_sorts import *
 
+# conduct_AverageRuns conducts an average number of runs for the specific sorting algorithm and measures the corresponding runtime
+def conduct_AverageRuns(averageRunCount, sortingAlgFunc, target_List):
+    algTime_sum = 0
 
+    for num in range(averageRunCount):
+        start_time = timeit.default_timer()
+        sortingAlgFunc(target_List)
+        end_time = timeit.default_timer()
+        algTime_sum += (end_time - start_time)
+
+    return algTime_sum
+
+# test_allSorts tests the 3 sorting algorithms for a specific amount of test_runs and plots the corresponding runtime results from each algorithm
 def test_allSorts(test_runs, average_runs, increaseList_factor):
     list_max = 100
     list_len = 100
@@ -14,41 +26,26 @@ def test_allSorts(test_runs, average_runs, increaseList_factor):
     for num in range(test_runs):
         L = create_random_list(list_len, list_max)
         list_lens.append(len(L))
-        Insertion_time_sum = 0
-        Bubble_time_sum = 0
-        Selection_time_sum = 0
-
-        for i in range(average_runs):
-            start_1 = timeit.default_timer()
-            insertion_sort(L)
-            end_1 = timeit.default_timer()
-            Insertion_time_sum += (end_1-start_1)
-
-            start_2 = timeit.default_timer()
-            bubble_sort(L)
-            end_2 = timeit.default_timer()
-            Bubble_time_sum += (end_2 - start_2)
-
-            start_3 = timeit.default_timer()
-            selection_sort(L)
-            end_3 = timeit.default_timer()
-            Selection_time_sum += (end_3 - start_3)
+        Insertion_time_sum = conduct_AverageRuns(average_runs, insertion_sort, L)
+        Bubble_time_sum = conduct_AverageRuns(average_runs, bubble_sort, L)
+        Selection_time_sum = conduct_AverageRuns(average_runs, selection_sort, L)
 
         Insertion_times.append(Insertion_time_sum/average_runs)
         Bubble_times.append(Bubble_time_sum / average_runs)
         Selection_times.append(Selection_time_sum/ average_runs)
         list_len += increaseList_factor
-        #print(times)
 
-    plot.plot(list_lens, Insertion_times, marker='o')
-    plot.plot(list_lens, Selection_times, marker='o')
-    plot.plot(list_lens, Bubble_times, marker='o')
-    plot.xlabel('List Length')
+
+    plot.plot(list_lens, Insertion_times, marker='o', label='Insertion Sort')
+    plot.plot(list_lens, Selection_times, marker='o', label=' Selection Sort')
+    plot.plot(list_lens, Bubble_times, marker='o', label=' Bubble Sort')
+    plot.legend(loc='upper left', title='Sorting Algorithms', fontsize=12, facecolor='lightgray')
+    plot.xlabel('List Length (n elements)')
     plot.ylabel('Runtime (seconds)')
     plot.title('Insertion, Bubble and Selection Sort Runtime vs List Length')
     plot.show()
-    return None
+    return Insertion_times, Bubble_times, Selection_times
 
 
 
-test_allSorts(10, 10, 50)
+test_allSorts(1000, 100, 1000)
