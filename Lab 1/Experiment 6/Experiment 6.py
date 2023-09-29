b7b1c546a67576598a1834e602484110bcb769d2
Ashwin Unnithan <unnithanash@gmail.com>
2023-09-29 16:38:16 -0400

    Add Exp8 (most of)

--- Lab 1/Experiment 6/Experiment 6.py
+++ Lab 1/Experiment 6/Experiment 6.py
@@ -1,6 +1,8 @@
 # ******************* Experiment 6 Functions *******************
 # Functions designated to running experiment 6 pertaining to verifying a potential speedup of quick sort
 
+# TODO maybe add partially sorted and sorted lists to test as well
+
 # Import the necessary modules/libraries needed for experiment
 from good_sorts import quicksort
 from bad_sorts import create_random_list
