f62830c883bd71f16eb4df88b8f6c8b53a2a175f
Ashwin Unnithan <unnithanash@gmail.com>
2023-09-29 12:19:54 -0400

    Add Exp6

---
+++ Lab 1/Experiment 6/Experiment 6.py
@@ -0,0 +1,106 @@
+# ******************* Experiment 6 Functions *******************
+# Functions designated to running experiment 8 pertaining to verifying a potential speedup of quick sort
+
+from good_sorts import quicksort
+from bad_sorts import create_random_list
+import timeit
+import matplotlib.pyplot as plot
+
+
+def print_DualQuickSortResult(L):
+    copy = dual_quicksort(L)
+    for i in range(len(L)):
+        L[i] = copy[i]
+    print(copy)
+
+
+# conduct_AverageRuns conducts an average number of runs for the specific sorting algorithm and measures the corresponding runtime of the algorithm (aginest a target list)
+# Paramaters: averageRunCount (number of average runs),  sortingAlgFunc (specific sorting algorithm function) and target_List(specific list to be used)
+def conduct_AverageRuns(averageRunCount, sortingAlgFunc, target_List):
+    algTime_sum = 0     #sum variable to store sum of runtime for the sorting alg
+
+    # iterates over a specified amount of runs and obtain a average runtime for the sorting algorithm
+    for num in range(averageRunCount):
+        start_time = timeit.default_timer()     # starts the timer
+        sortingAlgFunc(target_List.copy())             # calls the sort alg function with a copy of the list
+        end_time = timeit.default_timer()       # ends the time
+        algTime_sum += (end_time - start_time)  # adds the runtime to the algTime_sum
+
+    return algTime_sum      # returns the sum of the average runtimes
+
+
+# dual_quicksort(L) sorts a list using two pivots (first and second elements of the list respectively) via recursion
+def dual_quicksort(L):
+    if len(L) < 2:
+        return L
+    first_pivot = L[0]
+    sec_pivot = L[1]
+    left, middle, right = [], [], []
+
+    if(first_pivot > sec_pivot):
+        first_pivot = L[1]
+        sec_pivot = L[0]
+
+    # may not need this condition
+    else:
+        first_pivot = L[0]
+        sec_pivot = L[1]
+
+    for num in L[2:]:
+        if num < first_pivot:
+            left.append(num)
+        elif num > sec_pivot:
+            right.append(num)
+        else:
+            middle.append(num)
+
+    return dual_quicksort(left) + [first_pivot] + dual_quicksort(middle) + [sec_pivot] + dual_quicksort(right)
+
+
+# DualvsSingle_QuickSort tests dual quicksort vs. normal quicksort for a specific amount of test_runs and plots the corresponding runtime results from each algorithm using matplotlib
+# Paramaters: test_runs(number of actual test runs), average_runs (number of average runs - done during each actual test run) and increaseList_factor(factor used to increase the list size)
+def DualvsSingle_QuickSort(test_runs, average_runs, increaseList_factor):
+    list_max = 100
+    list_len = 0
+    # declare the default max and len of list: 0
+    # TODO maybe increase list max as well
+
+    # lists to store the average runtimes computed for each sorting alg as well as the increasing lengths of the list
+    SingleQuick_times = []
+    DualQuick_times = []
+    list_lens = []
+
+    # iterates over the specified number of test runs, creates a new list in each run and conducts average runs for dual quicksort and normal quicksort
+    for num in range(test_runs):
+        L = create_random_list(list_len, list_max)
+        SingleQuick_time_sum = conduct_AverageRuns(average_runs, quicksort, L)
+        DualQuick_time_sum = conduct_AverageRuns(average_runs, dual_quicksort, L)
+
+        # appends the target list length and runtimes of the sorting algs to the corresponding time lusts
+        list_lens.append(len(L))
+        SingleQuick_times.append(SingleQuick_time_sum/average_runs)
+        DualQuick_times.append(DualQuick_time_sum/ average_runs)
+
+        # increases the list's length by a corresponding factor
+        list_len += increaseList_factor
+
+
+    # plots the runtimes of the 3 sorting algorithms with the following legends and axis titles
+    plot.plot(list_lens, SingleQuick_times, label='Single Pivot QuickSort')
+    plot.plot(list_lens, DualQuick_times, label='Dual Pivot Quicksort')
+    plot.legend(loc='upper left', title='Quicksort Algorithms', fontsize=10, facecolor='lightgray')
+    plot.xlabel('List Length (n elements)')
+    plot.ylabel('Runtime (seconds)')
+    plot.title("Dual and Single Quicksort's Runtime vs List Length")
+    plot.show()
+
+    # returns the accumulated runtimes of all the sorting algs
+    return SingleQuick_times, DualQuick_times
+
+
+
+# calls the function to commence the test and plot the curve with the following parameters: 15 test runs, 30 average runs (for each test run) with an increasing list factor of 100
+DualvsSingle_QuickSort(15, 30, 100)
+
+
+
