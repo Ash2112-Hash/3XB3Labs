c4939947c33ec740634ee70a6d14b7b95a42b3ac
alexism-t <78742522+alexism-t@users.noreply.github.com>
2023-09-30 15:27:37 -0400

    Fixed exp 5, added images for exp 2, 4, 5

--- Lab 1/Experiment 5/Experiment 5.py
+++ Lab 1/Experiment 5/Experiment 5.py
@@ -5,37 +5,39 @@ import timeit
 import math
 import numpy as np
 
-length = 200
-num_swaps = int(length*math.log(length)/2)
-average_runs = 50
-
-def test_allSorts(length, num_swaps, average_runs, sortType):
+def test_allSorts(sortType):
+    length = 500
+    num_swaps = int(length*math.log(length, 10)/2)
+    max_value = 100
+    average_runs = 25
+    
     times = []
     swaps = []
-    time = 0
 
-    for i in range(num_swaps):
-        L = bad_sorts.create_near_sorted_list(length, 100, i)
-        swaps.append(i)
+    for i in range(0, num_swaps, 100): #resets timer for each algoirthm
+        time = 0
 
-        for i in range(average_runs):
+        for j in range(average_runs):
+            L = bad_sorts.create_near_sorted_list(length, max_value, i)
             start = timeit.default_timer()
-            sortType(L.copy())
+            sortType(L)
             end = timeit.default_timer()
             time += (end - start)
 
+        swaps.append(i)
         times.append(time / average_runs)
+
     return swaps, times
 
-swaps1, time1 = test_allSorts(length, num_swaps, average_runs, quicksort)
-swaps2, time2 = test_allSorts(length, num_swaps, average_runs, mergesort)
-swaps3, time3 = test_allSorts(length, num_swaps, average_runs, heapsort)
+swaps1, time1 = test_allSorts(quicksort)
+swaps2, time2 = test_allSorts(mergesort)
+swaps3, time3 = test_allSorts(heapsort)
 
 swap_quick, time_quick = np.array(swaps1), np.array(time1)
 swap_merge, time_merge = np.array(swaps2), np.array(time2)
 swap_heap, time_heap = np.array(swaps3), np.array(time3)
 
-plt.title("Number of Swaps vs Runtime")
+plt.title("Number of Swaps vs Runtime of Good Sorting Algorithms")
 plt.xlabel("Number of Swaps")
 plt.ylabel("Runtime (seconds)")
 
