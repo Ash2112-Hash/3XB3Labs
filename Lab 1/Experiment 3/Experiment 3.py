c96fad15b66047753d98cd7c9ca2c5757c06955f
ursulachui <chuiursula@gmail.com>
2023-09-30 13:16:07 -0400

    commented and refactored experiment 3 + finished partial experiment 7

--- Lab 1/Experiment 3/Experiment 3.py
+++ Lab 1/Experiment 3/Experiment 3.py
@@ -4,18 +4,24 @@ import numpy as np
 import math
 from bad_sorts import *
 
-num_swaps = int(1000*math.log(1000,10)/2)
-average_runs = 25
+def test_allSorts(sortType):
+    # chosen number of list length, max values of list, swaps and average runs
+    list_len = 1000
+    max_value = 100
+    average_runs = 25
+    num_swaps = int(list_len*math.log(list_len,10)/2)
 
-def test_allSorts(num_swaps, average_runs, sortType):
+    # lists to store the times and number of swaps performed for each sorting algorithm
     times = []
     swaps = []
 
+    # restarts the timer for every sorting algorithm
     for i in range(0, num_swaps, 100):
         time = 0
 
+        # running the experiment multiple times to obtain an accurate estimate
         for _ in range(average_runs):
-            L = create_near_sorted_list(1000, 100, i)
+            L = create_near_sorted_list(list_len, max_value, i)
             start = timeit.default_timer()
             sortType(L)
             end = timeit.default_timer()
@@ -25,18 +31,20 @@ def test_allSorts(num_swaps, average_runs, sortType):
 
     return swaps, times
 
-swaps1, time1 = test_allSorts(num_swaps, average_runs, insertion_sort)
-swaps2, time2 = test_allSorts(num_swaps, average_runs, bubble_sort)
-swaps3, time3 = test_allSorts(num_swaps, average_runs, selection_sort)
+# calls the function for the runtimes of the 3 sorting algorithms 
+swaps1, time1 = test_allSorts(insertion_sort)
+swaps2, time2 = test_allSorts(bubble_sort)
+swaps3, time3 = test_allSorts(selection_sort)
 
+# stores the results of the function calls in an array to display the data
 swap_insertion, time_insertion = np.array(swaps1), np.array(time1)
 swap_bubble, time_bubble = np.array(swaps2), np.array(time2)
 swap_selection, time_selection = np.array(swaps3), np.array(time3)
 
+# plotting the data and establishing the graph labels
 plt.title("Number of Swaps vs Runtime")
 plt.xlabel("Number of Swaps")
 plt.ylabel("Runtime (seconds)")
-
 plt.plot(swap_insertion, time_insertion, color = 'r', label = 'Insertion Sort')
 plt.plot(swap_bubble, time_bubble, color = 'b', label = 'Bubble Sort')
 plt.plot(swap_selection, time_selection, color = 'g', label = 'Selection Sort')
--- Lab 1/Experiment 7/Experiment 7.py
+++ Lab 1/Experiment 7/Experiment 7.py
@@ -1,8 +1,40 @@
-L = 
-size = len(L) // 100
+import timeit
+import matplotlib.pyplot as plt 
+import numpy as np
+import math
+from bad_sorts import *
+from good_sorts import *
+
+L = create_random_list(20,100)
 
 def bottom_up_mergesort(L):
-    for i in range()
-    if len(L) <= 1:
+    n = len(L)
+    size = 1
+
+    if n <= 1:
         return
     
+    while size < n:
+        for i in range(0, n, 2 * size):
+            left = L[i:i + size]
+            right = L[i + size:i + 2 * size]
+            merged = merge(left, right)
+            L[i:i + 2 * size] = merged
+        size *= 2
+
+def test_allSorts(firstSort, secondSort):
+    times = []
+    swaps = []
+
+    for i in range(0, num_swaps, 100):
+        time = 0
+
+        for _ in range(20):
+            L = create_near_sorted_list(1000, 100, i)
+            start = timeit.default_timer()
+            end = timeit.default_timer()
+            time += (end - start)
+        swaps.append(i)
+        times.append(time / 20)
+
+    return swaps, times
