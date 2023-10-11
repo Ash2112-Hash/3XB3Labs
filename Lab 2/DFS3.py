2097e24e00ee31ad7bd4eef0d933691cc1ed2fe8
ursulachui <chuiursula@gmail.com>
2023-10-11 11:07:32 -0400

    added DFS3

---
+++ Lab 2/DFS3.py
@@ -0,0 +1,18 @@
+from graph import *
+
+def BFS3(G, first_node):
+    S = [first_node]
+    marked = ()
+    predecessor = {}
+
+    while len(S) != 0:
+        current_node = S.pop()
+        if current_node not in marked:
+            marked.add(current_node)
+            for node in G.adj[first_node]:
+                if node not in marked:
+                    S.append(node)
+                    predecessor[node] = current_node
+
+    return predecessor
+
