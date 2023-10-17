44d495fd298b7e0473661c66e97ae361275a2bd7
Ashwin Unnithan <unnithanash@gmail.com>
2023-10-17 18:11:37 -0400

    Add cycleDetect

--- Lab 2/BFS3.py
+++ Lab 2/BFS3.py
@@ -2,13 +2,13 @@ from graph import *
 
 def BFS3(G, first_node):
     Q = deque(first_node)
-    marked = ()
+    marked = set()
     predecessor = {}
 
     while len(Q) != 0:
         current_node = Q.popleft()
         if current_node not in marked:
-            marked.update(current_node)
+            marked.add(current_node)
             for node in G.adj[first_node]:
                 if node not in marked:
                     Q.append(node)
--- Lab 2/DFS3.py
+++ Lab 2/DFS3.py
@@ -1,14 +1,14 @@
 from graph import *
 
-def BFS3(G, first_node):
+def DFS3(G, first_node):
     S = [first_node]
-    marked = ()
+    marked = set()
     predecessor = {}
 
     while len(S) != 0:
         current_node = S.pop()
         if current_node not in marked:
-            marked.update(current_node)
+            marked.add(current_node)
             for node in G.adj[first_node]:
                 if node not in marked:
                     S.append(node)
