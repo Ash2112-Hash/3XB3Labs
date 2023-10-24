1dba07dd3f0d2bcebffb0120a54dffd9e6e188eb
alexism-t <78742522+alexism-t@users.noreply.github.com>
2023-10-24 11:57:37 -0400

    Made C a set in all of the approximations

--- Lab 2/Exp1/vertexcover.py
+++ Lab 2/Exp1/vertexcover.py
@@ -2,7 +2,7 @@ import graph
 import random
 
 def approx1(G):
-    C = {}
+    C = set()
     degrees = []
     nodes = []
     for node in G:
@@ -10,29 +10,29 @@ def approx1(G):
         degree = #number of edges attached to node (this is the part i'm confused about LOL)
         degrees.append(degree)
     while graph.is_vertex_cover(G, C) == False:
-        C.append(max(degrees))
         i = degrees.index(max(degrees))
+        C.add(nodes[i])
         del nodes[i]
         del degrees[i]
     return C
 
 def approx2(G):
-    C = {}
+    C = set()
     nodes = []
-    for node in G:
+    for node in G.adj:
         nodes.append(node)
     while graph.is_vertex_cover(G, C) == False:
-        rand_node = random.randint(0, len(nodes))
-        C.append(nodes[rand_node])
+        rand_node = random.randint(0, len(nodes) - 1)
+        C.add(nodes[rand_node])
         del nodes[rand_node]
     return C
 
 def approx3(G):
-    C = {}
+    C = set()
     edges = []
     #get list of all the edges in G 
     while graph.is_vertex_cover(G, C) == False:
         rand_edge = random.randint(0, len(edges))
-        C.append(edges[rand_edge])
+        C.add(edges[rand_edge])
         del edges[rand_edge]
     return C
