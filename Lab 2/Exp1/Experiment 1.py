401edc1273901964c305079143ad7aab93b1c67e
Ashwin Unnithan <unnithanash@gmail.com>
2023-10-26 17:34:08 -0400

    Cleanup

--- Lab 2/Exp1/Experiment 1.py
+++ Lab 2/Exp1/Experiment 1.py
@@ -5,13 +5,13 @@
 import matplotlib.pyplot as plot
 from graph import *
 
-def plot_RandomGCycleProb(average_runs, node_count, edge_count):
+def plot_RandomGCycleProb(average_runs, node_count, edge_count, increase_edgeFactor):
     edge_quantities = []
     cycle_probs = []
     maxEdges = int(node_count * ((node_count-1)/2))
     num_edges = maxEdges if edge_count > maxEdges else edge_count
 
-    for edge_num in range(0, num_edges + 1, 2):
+    for edge_num in range(0, num_edges, increase_edgeFactor):
         cycleProbCounter = 0
 
         for _ in range(average_runs):
@@ -26,18 +26,19 @@ def plot_RandomGCycleProb(average_runs, node_count, edge_count):
         edge_quantities.append(edge_num)
         #print(cycle_probs)
 
-    plot.plot(edge_quantities, cycle_probs, label='Cycle Probability')
-    plot.xlabel('Number of Edges (n edges)')
+    plot.plot(edge_quantities, cycle_probs, label='Cycle Probability', marker = 'o')
+    plot.xlabel('Number of Edges (E edges)')
     plot.ylabel('Cycle Probability')
-    plot.title("Probability of Cycle within Random Graph based on Edge Quantity")
+    plot.title("Probability of Cycle within Random Graph(" + str(node_count) + " nodes) based on Edge Quantity")
     plot.show()
 
 
 def main():
-    plot_RandomGCycleProb(10, 10, 10)
-    plot_RandomGCycleProb(20, 50, 30)
-    plot_RandomGCycleProb(20, 60, 100)
-    plot_RandomGCycleProb(20, 60, 1000000)
+    plot_RandomGCycleProb(30, 10, 10, 2)
+    plot_RandomGCycleProb(200, 50, 30, 5)
+    plot_RandomGCycleProb(200, 50, 100, 5)
+    plot_RandomGCycleProb(500, 100, 300, 10)
+    plot_RandomGCycleProb(30, 10, 45, 2)
 
 
 main()
