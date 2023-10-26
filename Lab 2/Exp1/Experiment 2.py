73b1dc22fa8dadc5dbb43185e830f06147c48b83
Ashwin Unnithan <unnithanash@gmail.com>
2023-10-26 10:05:59 -0400

    Fix Exp2

--- Lab 2/Exp1/Experiment 2.py
+++ Lab 2/Exp1/Experiment 2.py
@@ -1,33 +1,30 @@
 import graph
 import matplotlib.pyplot as plt
 
+
 # ******Graphing*******
 
 def test_connected(num_nodes, num_edges, test_runs):
     probabilities = []
-    connected_or_not = []
     track_edges = []
+    maxEdges = int(num_nodes * ((num_nodes - 1) / 2))
+    edge_count = maxEdges if num_edges > maxEdges else num_edges
 
-    for i in range(0, num_edges + 1, 2):
+    for i in range(0, edge_count + 1, 2):
+        connected_ProbCounter = 0
 
         for j in range(test_runs):
             G = graph.create_random_graph(num_nodes, i)
-            connected = graph.Is_connected(G)
-            connected_or_not.append(connected)
 
-        counter = 0
-        for element in connected_or_not:
-            if element == True:
-                counter += 1
-        
-        probability = counter / len(connected_or_not)
-        probabilities.append(probability/test_runs)
+            if graph.Is_connected(G):
+                connected_ProbCounter += 1
 
+        probabilities.append(connected_ProbCounter / test_runs)
         track_edges.append(i)
 
-    #TODO may need to confirm if method is correct
+    # TODO may need to confirm if method is correct
 
-    #plotting the graph
+    # plotting the graph
     plt.plot(track_edges, probabilities)
     plt.xlabel('Number of Edges')
     plt.ylabel('Likelihood')
@@ -39,5 +36,6 @@ def main():
     test_connected(20, 60, 100)
     test_connected(40, 80, 100)
     test_connected(100, 200, 100)
+
 
 main()
