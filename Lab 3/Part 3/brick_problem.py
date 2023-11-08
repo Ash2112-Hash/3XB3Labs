3d6fcf8a0c88eaf7af6afe967d3ff06e175ac56a
alexism-t <78742522+alexism-t@users.noreply.github.com>
2023-11-08 02:46:28 -0500

    Finished first function of part 3

---
+++ Lab 3/Part 3/brick_problem.py
@@ -0,0 +1,35 @@
+MAX_INT = 32767
+
+
+def num_of_wc_runs(n, m):
+	#n is the number of bricks, m is the number of settings
+	brickTests = [[0 for i in range(m + 1)] for i in range(n + 1)] #bottom-up approach
+
+	# Base cases
+	for i in range(1, n + 1):
+		brickTests[i][1] = 1 #"If there's only 1 setting, there is only 1 setting to test (crazy I know)
+		brickTests[i][0] = 0 #We can assume with 0 settings we don't need to test
+
+	# "Case 2" as described in lab manual
+	for i in range(1, m + 1):
+		brickTests[1][i] = i
+
+	# Fill rest of the entries in table 
+	for i in range(2, n + 1):
+		for j in range(2, m + 1):
+			brickTests[i][j] = MAX_INT
+			for k in range(1, j + 1):
+				placeholder = 1 + max(brickTests[i-1][k-1], brickTests[i][j-k])
+				if placeholder < brickTests[i][j]:
+					brickTests[i][j] = placeholder
+
+	return brickTests[n][m]
+	
+def main():
+    #Testing
+    n = 2
+    m = 100
+    print("The number of trials needed in the worst case of", n, "bricks and", m, "test settings is", (num_of_wc_runs(n, m)))
+	
+main()
+
