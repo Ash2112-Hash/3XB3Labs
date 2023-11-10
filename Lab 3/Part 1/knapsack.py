f2f691b26c03d2f99a8ffe50ef60dc1e5ee0899f
ursulachui <chuiursula@gmail.com>
2023-11-10 00:55:00 -0500

    plotted BU and TD

--- Lab 3/Part 1/knapsack.py
+++ Lab 3/Part 1/knapsack.py
@@ -9,16 +9,6 @@ def createRandomTupleList(numElms, valMin, valMax, weightMin, weightMax):
     for _ in range(numElms):
         rand_W = random.randint(weightMin, weightMax)
         rand_val = random.randint(valMin, valMax)
-
-        if (rand_val, rand_W) in rand_list or (rand_W, rand_val) in rand_list:
-
-            while True:
-                NewRand_W = random.randint(weightMin, weightMax)
-                NewRand_val = random.randint(valMin, valMax)
-
-                if (rand_W, rand_val) != (NewRand_W, NewRand_val):
-                    rand_list.append((NewRand_W, NewRand_val))
-
         rand_list.append((rand_W, rand_val))
 
     return rand_list
@@ -33,10 +23,11 @@ def ks_top_down(items, capacity):
             return 0
         if matrix[i][capacity] != -1:
             return matrix[i][capacity]   
-            
+        # returning value of current item if previous item's weight that is less the capacity
         if items[i-1][0] <= capacity:
             matrix[i][capacity] = max(items[i-1][1] + knapsack(capacity - items[i-1][0], i-1), knapsack(capacity, i-1))
             return matrix[i][capacity]
+        # returning value of current item if previous item's weight that is more the capacity
         else:
             matrix[i][capacity] = knapsack(capacity, i-1)
             return matrix[i][capacity]
@@ -51,7 +42,7 @@ def ks_bottom_up(items, capacity):
             # base cases (when there is no weight or value, the profit is 0)
             if value == 0 or weight == 0:
                 matrix[value][weight] = 0
-            # filling in table with 
+            # filling in table for items that have previous weights that are less the current capacity
             elif items[value - 1][0] <= weight:
                 matrix[value][weight] = max(items[value - 1][1] + matrix[value - 1][weight - items[value - 1][0]], matrix[value - 1][weight])
             # same as previous value if weight of item being checked is more than the current capacity (column)
@@ -60,49 +51,74 @@ def ks_bottom_up(items, capacity):
 
     return matrix[len(items)][capacity]
 
-def conduct_AverageRuns(averageRunCount, dynamic_algorithm):
-    algTime_sum = 0    
+def TDvsBU(average_runs):
+    capacity = 50
+    valMin, weightMin = 0, 0
+    valMax, weightMax = 100, 100
 
-    for num in range(averageRunCount):
-        start_time = timeit.default_timer()    
-        dynamic_algorithm            
-        end_time = timeit.default_timer()      
-        algTime_sum += (end_time - start_time)  
-    return algTime_sum     
-
-def test_knapsack(test_num, average_runs, increase_factor):
-    numElms = 0
-    valMin = 0
-    weightMin = 0
-    valMax = 10
-    weightMax = 1000
-    capacity = 100
-
-    list_lens = []
+    list_lens = [n for n in range(1, 700)]
     BU_times = []
     TD_times = []
 
-    for num in range(test_num):
-        L = createRandomTupleList(numElms, valMin, valMax, weightMin, weightMax)
-        BU_sum = conduct_AverageRuns(average_runs, ks_bottom_up(L, capacity))
-        TD_sum = conduct_AverageRuns(average_runs, ks_top_down(L, capacity))
+    for n in list_lens:
+        BU_sum, TD_sum = 0, 0
 
-        list_lens.append(len(L))
+        for _ in range(average_runs):
+            items = createRandomTupleList(n, valMin, valMax, weightMin, weightMax)
+            
+            start_time = timeit.default_timer()
+            BU = ks_bottom_up(items, capacity)
+            end_time = timeit.default_timer()
+            BU_sum += (end_time - start_time)  
+
+            start_time = timeit.default_timer()
+            TD = ks_top_down(items, capacity)
+            end_time = timeit.default_timer()
+            TD_sum += (end_time - start_time)
+
         BU_times.append(BU_sum/average_runs)
-        TD_times.append(TD_sum/average_runs)
-
-        numElms += increase_factor
+        TD_times.append(TD_sum/average_runs)  
 
     plot.plot(list_lens, BU_times, label = 'Bottom Up Approach')
     plot.plot(list_lens, TD_times, label = 'Top Down Approach')
-    plot.legend(loc='upper left', title='Dynamic Programming Approach', fontsize=10)
+    plot.legend(loc = 'upper left', title = 'Dynamic Programming Approach', fontsize=10)
     plot.xlabel('Number of Items (n elements)')
     plot.ylabel('Runtime (seconds)')
     plot.title("Runtime vs Number of Items")
     plot.show()
 
-    return BU_times, TD_times
+def BUvsTD(average_runs):
+    capacity = 50
+    valMin, weightMin = 0, 0
+    valMax, weightMax = 2000, 2000
 
-test_knapsack(5, 1, 10)
+    list_lens = [n for n in range(1, 500)]
+    BU_times = []
+    TD_times = []
 
+    for n in list_lens:
+        BU_sum, TD_sum = 0, 0
 
+        for _ in range(average_runs):
+            items = createRandomTupleList(n, valMin, valMax, weightMin, weightMax)
+            
+            start_time = timeit.default_timer()
+            BU = ks_bottom_up(items, capacity)
+            end_time = timeit.default_timer()
+            BU_sum += (end_time - start_time)  
+
+            start_time = timeit.default_timer()
+            TD = ks_top_down(items, capacity)
+            end_time = timeit.default_timer()
+            TD_sum += (end_time - start_time)
+
+        BU_times.append(BU_sum/average_runs)
+        TD_times.append(TD_sum/average_runs)  
+
+    plot.plot(list_lens, BU_times, label = 'Bottom Up Approach')
+    plot.plot(list_lens, TD_times, label = 'Top Down Approach')
+    plot.legend(loc = 'upper left', title = 'Dynamic Programming Approach', fontsize=10)
+    plot.xlabel('Number of Items (n elements)')
+    plot.ylabel('Runtime (seconds)')
+    plot.title("Runtime vs Number of Items")
+    plot.show()
