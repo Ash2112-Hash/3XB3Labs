3cfe75214501d40837c00cd3413b0995afe4dfb4
Ashwin Unnithan <unnithanash@gmail.com>
2023-11-09 20:16:29 -0500

    Add brute force solution

--- Lab 3/Part 1/knapsack.py
+++ Lab 3/Part 1/knapsack.py
@@ -1,10 +1,10 @@
 # Knapsack Functions
 import random
-
+import itertools
 
 
 def createRandomTupleSet(numElms, valMin, valMax, weightMin, weightMax):
-    rand_set = set()
+    rand_set = []
 
     for _ in range(numElms):
         rand_W = random.randint(weightMin, weightMax)
@@ -17,19 +17,30 @@ def createRandomTupleSet(numElms, valMin, valMax, weightMin, weightMax):
                 NewRand_val = random.randint(valMin, valMax)
 
                 if (rand_W, rand_val) != (NewRand_W, NewRand_val):
-                    rand_set.add((NewRand_W, NewRand_val))
+                    rand_set.append((NewRand_W, NewRand_val))
 
-        rand_set.add((rand_W, rand_val))
+        rand_set.append((rand_W, rand_val))
 
     return rand_set
 
 
 
-
 def ks_brute_force(items, capacity):
-    pass
+    max_val = 0
+    initial_subsets = []
+    item_subsets = []
 
+    for i in range(len(items) + 1):
+        initial_subsets.extend(itertools.combinations(items, i))
 
+    for each_subset in initial_subsets:
+        item_subsets.append(list(each_subset))
 
-L = createRandomTupleSet(10, 0, 10, 0, 100)
-print(L)
+    for each_subset in item_subsets:
+        new_W = sum(item_tuple[0] for item_tuple in each_subset)
+        new_value = sum(item_tuple[1] for item_tuple in each_subset)
+
+        if new_W <= capacity and new_value > max_val:
+            max_val = new_value
+
+    return max_val
