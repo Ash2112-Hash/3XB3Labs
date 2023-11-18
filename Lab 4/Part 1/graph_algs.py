833399c576bcb9f199b887661158e13c4a2db784
Ashwin Unnithan <unnithanash@gmail.com>
2023-11-18 17:41:57 -0500

    Add new kDijkstra

--- Lab 4/Part 1/graph_algs.py
+++ Lab 4/Part 1/graph_algs.py
@@ -3,8 +3,34 @@ from final_project_part1 import *
 
 
 def dijkstra_approx(G, source, k):
-    pass
+    dist = {}
+    Q = min_heap.MinHeap([])
+    G_nodes = list(G.adj.keys())
+    relaxedCount = k
+
+    for node in G_nodes:
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+    Q.decrease_key(source, 0)
+
+    while not Q.is_empty() and relaxedCount != 0:
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        dist[current_node] = current_element.key
+
+        for neighbour in G.adj[current_node]:
+            if (dist[current_node] + G.w(current_node, neighbour)) < dist[neighbour]:
+                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
+                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+
+        relaxedCount-=1
+
+    return dist
 
 
-def bellman_ford_approx(G, source, k):
-    pass
+
+G = create_random_complete_graph(10, 200)
+print(G.adj)
+print(G.weights)
+print(dijkstra(G, 0))
+print(dijkstra_approx(G, 0, 1))
