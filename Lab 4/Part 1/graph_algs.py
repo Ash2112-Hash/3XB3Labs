a71373a84079f5d0eced07ba328b2afb31ca753f
Ashwin Unnithan <unnithanash@gmail.com>
2023-11-18 20:45:05 -0500

    Add k BF

--- Lab 4/Part 1/graph_algs.py
+++ Lab 4/Part 1/graph_algs.py
@@ -6,36 +6,61 @@ def dijkstra_approx(G, source, k):
     dist = {}
     Q = min_heap.MinHeap([])
     G_nodes = list(G.adj.keys())
-    relaxedCount = k
+    relaxedCount = {}
 
     for node in G_nodes:
         Q.insert(min_heap.Element(node, float("inf")))
         dist[node] = float("inf")
+        relaxedCount[node] = 0
     Q.decrease_key(source, 0)
 
-    while not Q.is_empty() and relaxedCount != 0:
+    while not Q.is_empty():
         current_element = Q.extract_min()
         current_node = current_element.value
         dist[current_node] = current_element.key
 
-        for neighbour in G.adj[current_node]:
-            if (dist[current_node] + G.w(current_node, neighbour)) < dist[neighbour]:
-                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
-                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+        if relaxedCount[current_node] < k:
+            for neighbour in G.adj[current_node]:
+                if (dist[current_node] + G.w(current_node, neighbour)) < dist[neighbour]:
+                    Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
+                    dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                    relaxedCount[neighbour] += 1
+        else:
+            continue
 
-        relaxedCount -= 1
+    return dist
+
+
+def bellman_ford_approx(G, source, k):
+    dist = {}
+    G_nodes = list(G.adj.keys())
+    relaxedCount = {}
+
+    for node in G_nodes:
+        dist[node] = float("inf")
+        relaxedCount[node] = 0
+    dist[source] = 0
+
+    for _ in range(G.number_of_nodes()):
+        for node in G_nodes:
+            if relaxedCount[node] < k:
+                for neighbour in G.adj[node]:
+                    if dist[neighbour] > dist[node] + G.w(node, neighbour):
+                        dist[neighbour] = dist[node] + G.w(node, neighbour)
+                        relaxedCount[neighbour] += 1
+            else:
+                continue
 
     return dist
 
 
 
-
-
-
+"""TESTING"""
 for _ in range(100):
     G = create_random_complete_graph(10, 200)
     print(G.adj)
     print(G.weights)
     print(dijkstra(G, 0))
     print(dijkstra_approx(G, 0, 1))
+    print(bellman_ford_approx(G, 0, 1))
     assert dijkstra(G, 0) != dijkstra_approx(G, 0, 1)
