5365c04941f33f7673c3a588a881ca5ec6849235
ursulachui <chuiursula@gmail.com>
2023-11-28 11:19:49 -0500

    a star algorithm

---
+++ Lab 4/Part 1/a_star.py
@@ -0,0 +1,44 @@
+from final_project_part1 import *
+
+def total_cost(h, dist, node_cost):
+    cost = {}
+
+    # add the distance and heuristic function
+    for i in node_cost:
+        cost[node_cost[i]] = dist[i] + h[i]
+
+    # sort nodes by second element which contains the total cost 
+    sorted_dict = sorted(node_cost, key=lambda cost: cost[1])
+
+    # loop through the sorted dictionary and only extract the node number
+    for node in sorted_dict:
+        return node[0]
+
+# using Dijkstra's with an addition of h 
+def a_star(G, s, d, h):
+    pred = {}
+    dist = {}
+    Q = min_heap.MinHeap([])
+    nodes = list(G.adj.keys())
+
+    for node in nodes:
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+    Q.decrease_key(s, 0)
+
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        dist[current_node] = current_element.key
+
+        # case where node being searched is starting node
+        if current_node == d:
+            break
+
+        node_path = total_cost(h, dist, G.adj[current_node])
+        for neighbour in node_path:
+            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
+                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
+                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                pred[neighbour] = current_node
+    return pred, dist
