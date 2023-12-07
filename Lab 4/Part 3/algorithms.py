2de170225a8aefcd912717c141ca8a50bf29080c
ursulachui <chuiursula@gmail.com>
2023-12-06 22:38:39 -0500

    fixed transfer check

--- Lab 4/Part 3/algorithms.py
+++ Lab 4/Part 3/algorithms.py
@@ -1,7 +1,5 @@
 import min_heap
 
-from final_project_part1 import *
-
 def total_cost(h, dist, node_cost):
     cost = {}
     cost_order = []
@@ -19,8 +17,37 @@ def total_cost(h, dist, node_cost):
 
     return cost_order
 
-# using Dijkstra's with an addition of the heuristic 
 def a_star(G, s, d, h):
+    pred = {}
+    dist = {}
+    Q = min_heap.MinHeap([])
+    nodes = list(G.adj.keys())
+
+    for node in nodes:
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+    Q.decrease_key(s, 0)
+
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        dist[current_node] = current_element.key
+
+        # case where node being searched is starting node
+        if current_node == d:
+            break
+
+        node_path = total_cost(h, dist, G.adj[current_node])
+        for neighbour in node_path:
+            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
+                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
+                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                pred[neighbour] = current_node
+
+    return dist
+
+# using Dijkstra's with an addition of the heuristic 
+def a_star_new(G, s, d, h):
     pred = {}
     dist = {}
     Q = min_heap.MinHeap([])
